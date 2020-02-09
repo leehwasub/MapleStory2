216ab5594be4a4bc0120d74a899d744140ced54b
leehwasub <hwasub1115@naver.com>
2020-02-09 16:28:20 +0900

    commit..

--- src/npc/Starling.java
+++ src/npc/Starling.java
@@ -25,7 +25,7 @@ public class Starling extends Npc{
 		if(questNum == 0) {
 			quest = Quest.makeQuest(1, 14, "에레고스 처치 준비").addMessage("폐쇄 구역에서 '세릴'을 만났다. 에릴의 말에 의하면 에레고스를 처치하기 위해서는 샤레니안 궁전에 가야하고 에레고스를 봉인하고 있는 샤레니안 궁전의 결계를")
 					.addMessage("부수지 않고 들어가기 위해서는 마법 포탈을 만들 필요가 있다고 한다. 에릴은 마법사가 아니기때문에 재료가 있어야 마법포탈을 만들 수 있다고 하는데. 마법 포탈을 만들기 위해 폐쇄 구역과 군영에서 나오는 몬스터들의 재료를 모아가면 될것같다.")
-					.addQuestMaterial("갈비뼈", 10).addQuestMaterial("골반뼈", 10).addQuestMaterial("말머리뼈", 5).setRewardExp(1000).setRewardMoney(2000)
+					.addQuestMaterial("갈비뼈", 10).addQuestMaterial("골반뼈", 10).addQuestMaterial("말머리뼈", 5).setRewardExp(1200).setRewardMoney(2400)
 					.addRewardUpdateMap("폐쇄구역", 0, 24, MapleMap.MAP_PORTAL_STATE).addRewardItem("장어구이", 50)
 					.setPlayerQuestProceed(QuestProceed.STARLING_QUEST_1);
 			player.setQuest(quest);
--- src/npc/TenBoogies.java
+++ src/npc/TenBoogies.java
@@ -41,7 +41,7 @@ public class TenBoogies extends Npc{
 		if(questNum == 0) {
 			quest = Quest.makeQuest(1, 10, "우드마스크 조사").addMessage("페리온에서 유물 발굴단의 일원인 '열 마리의 부기를'만났다. 그녀는 최근 페리온 주변에서 마스크를 쓰고 다니는 이상한 몬스터가 발견 되고 있다고 한다.")
 					.addMessage("이 몬스터들을 잡아서 표본을 조사해야 원인이 되는 실마리를 찾을수 있을것 같다는데.").addQuestKill("우드마스크", 15)
-					.setRewardExp(400).setRewardMoney(700).setPlayerQuestProceed(QuestProceed.TENBOOGIES_QUEST_1)
+					.setRewardExp(600).setRewardMoney(1200).setPlayerQuestProceed(QuestProceed.TENBOOGIES_QUEST_1)
 					.addRewardNpcQuestProceed("윈스턴", 1);
 			player.setQuest(quest);
 		} else if(questNum == 1) {
